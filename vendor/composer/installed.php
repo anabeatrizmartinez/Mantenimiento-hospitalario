6f2d665d0dda5bf406a1e932dcf3de0cf1713d6b
Ana Beatriz <anabemahe@gmail.com>
2020-08-26 23:17:52 -0400

    Installing composer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '50611cc39ac94f7ea55374d65b5a52157e304b00',
+    'name' => 'trabajo/una',
+  ),
+  'versions' => 
+  array (
+    'trabajo/una' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '50611cc39ac94f7ea55374d65b5a52157e304b00',
+    ),
+  ),
+);
